4817ae5253ad24a9a6e996143503a2dff1433960
Elli-tt <elliottjoiner@me.com>
2020-10-10 18:03:39 +0100

    ouraniaaltar: fixed overlay bug

--- ouraniaaltar/src/main/java/net/runelite/client/plugins/ouraniaaltar/ouraniaaltarOverlay.java
+++ ouraniaaltar/src/main/java/net/runelite/client/plugins/ouraniaaltar/ouraniaaltarOverlay.java
@@ -54,9 +54,9 @@ class ouraniaaltarOverlay extends OverlayPanel
         timeFormat = (duration.toHours() < 1) ? "mm:ss" : "HH:mm:ss";
         tableComponent.addRow("Time:", formatDuration(duration.toMillis(), timeFormat));
         if(plugin.status!=null){
-            tableComponent.addRow("Status:", "Starting...");
-        } else {
             tableComponent.addRow("Status:", plugin.status.toString());
+        } else {
+            tableComponent.addRow("Status:", "Starting...");
         }
         tableComponent.addRow("Progress: ", String.valueOf(plugin.runecraftProgress));
         tableComponent.addRow("Essence: ", plugin.currentEss + " (-" + (plugin.startEss - plugin.currentEss) + ")");
