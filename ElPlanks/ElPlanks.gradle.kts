da3aeb3ecb4ffddbe02109f01cdf392d9557916e
Elli-tt <elliottjoiner@me.com>
2020-10-18 15:34:18 +0100

    ElPlanks 1.0.0: initial release ElTest 1.0.4: some widget stuff added

--- ElPlanks/ElPlanks.gradle.kts
+++ ElPlanks/ElPlanks.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Owain van Brakel <https:github.com/Owain94>
+ * Copyright (c) 2019 Owain van Brakel <https://github.com/Owain94>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,32 +23,29 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "Elliott Plugins"
+version = "1.0.0"
 
-include(":superglassmaker")
-include(":glassblower")
-include(":ouraniaaltar")
-include(":firemaker")
-include(":bloodrunecrafter")
-include(":sandstoneminer")
-include(":karambwanfisher")
-include(":cooker")
-include(":barbarianfisher")
-include(":tickcooker")
-include(":ElBankStander")
-include(":ElAstrals")
-include(":ElTest")
-include(":ElHunter")
-//include(":ElCosmics")
-include(":ElTutorial")
-include(":ElPlanks")
+project.extra["PluginName"] = "El Planks" // This is the name that is used in the external plugin manager panel
+project.extra["PluginDescription"] = "Makes plank in your POH" // This is the description that is used in the external plugin manager panel
 
-for (project in rootProject.children) {
-    project.apply {
-        projectDir = file(name)
-        buildFileName = "$name.gradle.kts"
+dependencies {
+    compileOnly(group = "com.openosrs.externals", name = "botutils", version = "4.9.1+");
+}
 
-        require(projectDir.isDirectory) { "Project '${project.path} must have a $projectDir directory" }
-        require(buildFile.isFile) { "Project '${project.path} must have a $buildFile build script" }
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Dependencies" to
+                            arrayOf(
+                                    "botutils-plugin"
+                            ).joinToString(),
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
     }
 }
--- ElTest/src/main/java/net/runelite/client/plugins/ElTest/ElTestPlugin.java
+++ ElTest/src/main/java/net/runelite/client/plugins/ElTest/ElTestPlugin.java
@@ -8,10 +8,13 @@ import net.runelite.api.Point;
 import net.runelite.api.coords.WorldArea;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.*;
+import net.runelite.api.widgets.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.SpriteManager;
+import net.runelite.client.game.SpriteOverride;
 import net.runelite.client.input.KeyListener;
 import net.runelite.client.input.KeyManager;
 import net.runelite.client.input.MouseListener;
@@ -21,6 +24,8 @@ import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.util.ImageUtil;
 import org.pf4j.Extension;
 import net.runelite.client.plugins.botutils.BotUtils;
 import java.awt.*;
@@ -32,6 +37,7 @@ import java.net.*;
 import java.time.Instant;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static net.runelite.client.plugins.ElTest.ElTestState.*;
 
@@ -71,6 +77,11 @@ public class ElTestPlugin extends Plugin implements MouseListener, KeyListener {
 	@Inject
 	private KeyManager keyManager;
 
+	@Inject
+	private ClientThread clientThread;
+
+	@Inject
+	private SpriteManager spriteManager;
 
 	int clientTickBreak = 0;
 	int tickTimer;
@@ -79,8 +90,16 @@ public class ElTestPlugin extends Plugin implements MouseListener, KeyListener {
 
 	Instant botTimer;
 
+	private Widget picker = null;
+	private Widget protMelee = null;
+	private Widget bankEniola = null;
+	private Widget ouraniaTele = null;
+
 	int clientTickCounter;
 	boolean clientClick;
+
+	GameObject targetObject;
+	NPC targetNpc;
 
 
 	// Provides our config
@@ -99,6 +118,10 @@ public class ElTestPlugin extends Plugin implements MouseListener, KeyListener {
 		setValues();
 		startTest=false;
 		log.info("Plugin started");
+		clientThread.invoke(this::addPickerWidget);
+		//clientThread.invoke(this::addMeleeWidget);
+		clientThread.invoke(this::addOuraniaTeleWidget);
+		clientThread.invoke(this::addBankEniolaWidget);
 	}
 
 	@Override
@@ -110,6 +133,10 @@ public class ElTestPlugin extends Plugin implements MouseListener, KeyListener {
 		setValues();
 		startTest=false;
 		log.info("Plugin stopped");
+		clientThread.invoke(this::removePickerWidget);
+		//clientThread.invoke(this::removeMeleeWidget);
+		clientThread.invoke(this::removeOuraniaTeleWidget);
+		clientThread.invoke(this::removeBankEniolaWidget);
 	}
 
 	@Subscribe
@@ -185,10 +212,257 @@ public class ElTestPlugin extends Plugin implements MouseListener, KeyListener {
 		}
 	}
 
+	private void addPickerWidget()
+	{
+		removePickerWidget();
+
+		int x = 0, y = 0;
+		Widget parent = client.getWidget(161,16);
+		if (parent == null)
+		{
+			Widget[] roots = client.getWidgetRoots();
+			parent = Stream.of(roots)
+					.filter(w -> w.getType() == WidgetType.LAYER && w.getContentType() == 0 && !w.isSelfHidden()).max(Comparator.comparing((Widget w) -> w.getRelativeX() + w.getRelativeY())
+							.reversed()
+							.thenComparing(Widget::getId)).get();
+			x = 4;
+			y = 4;
+		}
+		picker = parent.createChild(-1, WidgetType.GRAPHIC);
+
+		log.info("Picker is {}.{} [{}]", WidgetInfo.TO_GROUP(picker.getId()), WidgetInfo.TO_CHILD(picker.getId()), picker.getIndex());
+
+		client.getSpriteOverrides().put(-300, ImageUtil.getImageSprite(ImageUtil.getResourceStreamFromClass(ElTestPlugin.class, "ouraniachin.png"), client));
+		picker.setSpriteId(-300);
+		picker.setOriginalWidth(30);
+		picker.setOriginalHeight(30);
+		picker.setOriginalX(parent.getWidth()-30);
+		picker.setOriginalY(parent.getHeight()-30);
+		picker.revalidate();
+		picker.setTargetVerb("action3");
+		picker.setName("button3");
+		picker.setClickMask(WidgetConfig.USE_WIDGET);
+		picker.setNoClickThrough(true);
+	}
+
+	private void removePickerWidget()
+	{
+		if (picker == null)
+		{
+			return;
+		}
+
+		Widget parent = picker.getParent();
+		if (parent == null)
+		{
+			return;
+		}
+
+		Widget[] children = parent.getChildren();
+		if (children == null || children.length <= picker.getIndex() || children[picker.getIndex()] != picker)
+		{
+			return;
+		}
+
+		children[picker.getIndex()] = null;
+	}
+
+	private void addOuraniaTeleWidget()
+	{
+		removeOuraniaTeleWidget();
+		Widget parent = client.getWidget(161,16);
+		int x = 0, y = 0;
+		if (parent == null)
+		{
+			Widget[] roots = client.getWidgetRoots();
+
+			parent = Stream.of(roots)
+					.filter(w -> w.getType() == WidgetType.GRAPHIC && w.getContentType() == 0 && !w.isSelfHidden()).max(Comparator.comparing((Widget w) -> w.getRelativeX() + w.getRelativeY())
+							.reversed()
+							.thenComparing(Widget::getId)).get();
+			x = 4;
+			y = 4;
+		}
+
+		ouraniaTele = parent.createChild(-1, WidgetType.GRAPHIC);
+
+		//client.getSpriteOverrides().put(-300, ImageUtil.getImageSprite(ImageUtil.getResourceStreamFromClass(ElTestPlugin.class, "zmibutton.png"), client));
+		ouraniaTele.setSpriteId(SpriteID.SPELL_OURANIA_TELEPORT);
+		ouraniaTele.setOriginalWidth(30);
+		ouraniaTele.setOriginalHeight(30);
+		ouraniaTele.setOriginalX(parent.getWidth()-30);
+		ouraniaTele.setOriginalY(parent.getHeight()-65);
+		ouraniaTele.revalidate();
+		ouraniaTele.setTargetVerb("action2");
+		ouraniaTele.setName("button2");
+		ouraniaTele.setClickMask(WidgetConfig.USE_WIDGET);
+		ouraniaTele.setNoClickThrough(true);
+	}
+
+	private void removeOuraniaTeleWidget()
+	{
+		if (ouraniaTele == null)
+		{
+			return;
+		}
+
+		Widget parent = ouraniaTele.getParent();
+		if (parent == null)
+		{
+			return;
+		}
+
+		Widget[] children = parent.getChildren();
+		if (children == null || children.length <= ouraniaTele.getIndex() || children[ouraniaTele.getIndex()] != ouraniaTele)
+		{
+			return;
+		}
+
+		children[ouraniaTele.getIndex()] = null;
+	}
+
+	private void addBankEniolaWidget()
+	{
+		removeBankEniolaWidget();
+		Widget parent = client.getWidget(161,16);
+		int x = 0, y = 0;
+		if (parent == null)
+		{
+			Widget[] roots = client.getWidgetRoots();
+
+			parent = Stream.of(roots)
+					.filter(w -> w.getType() == WidgetType.GRAPHIC && w.getContentType() == 0 && !w.isSelfHidden()).max(Comparator.comparing((Widget w) -> w.getRelativeX() + w.getRelativeY())
+							.reversed()
+							.thenComparing(Widget::getId)).get();
+			x = 4;
+			y = 4;
+		}
+
+		bankEniola = parent.createChild(-1, WidgetType.GRAPHIC);
+
+		client.getSpriteOverrides().put(-301, ImageUtil.getImageSprite(ImageUtil.getResourceStreamFromClass(ElTestPlugin.class, "eniola.png"), client));
+		bankEniola.setSpriteId(-301);
+		bankEniola.setOriginalWidth(30);
+		bankEniola.setOriginalHeight(30);
+		bankEniola.setOriginalX(parent.getWidth()-30);
+		bankEniola.setOriginalY(parent.getHeight()-100);
+		bankEniola.revalidate();
+		bankEniola.setTargetVerb("action1");
+		bankEniola.setName("button1");
+		bankEniola.setClickMask(WidgetConfig.USE_WIDGET);
+		bankEniola.setNoClickThrough(true);
+	}
+
+	private void removeBankEniolaWidget()
+	{
+		if (bankEniola == null)
+		{
+			return;
+		}
+
+		Widget parent = bankEniola.getParent();
+		if (parent == null)
+		{
+			return;
+		}
+
+		Widget[] children = parent.getChildren();
+		if (children == null || children.length <= bankEniola.getIndex() || children[bankEniola.getIndex()] != bankEniola)
+		{
+			return;
+		}
+
+		children[bankEniola.getIndex()] = null;
+	}
+
+	/*private void addMeleeWidget()
+	{
+		removeMeleeWidget();
+
+
+		Widget parent = client.getWidget(161,16);
+		int x = 0, y = 0;
+		if (parent == null)
+		{
+			Widget[] roots = client.getWidgetRoots();
+
+			parent = Stream.of(roots)
+					.filter(w -> w.getType() == WidgetType.GRAPHIC && w.getContentType() == 0 && !w.isSelfHidden()).max(Comparator.comparing((Widget w) -> w.getRelativeX() + w.getRelativeY())
+							.reversed()
+							.thenComparing(Widget::getId)).get();
+			x = 4;
+			y = 4;
+		}
+
+		protMelee = parent.createChild(-1, WidgetType.GRAPHIC);
+
+		//client.getSpriteOverrides().put(-300, ImageUtil.getImageSprite(ImageUtil.getResourceStreamFromClass(ElTestPlugin.class, "zmibutton.png"), client));
+		protMelee.setSpriteId(SpriteID.PRAYER_PROTECT_FROM_MELEE);
+		protMelee.setOriginalWidth(30);
+		protMelee.setOriginalHeight(30);
+		protMelee.setOriginalX(parent.getWidth()-50);
+		protMelee.setOriginalY(parent.getHeight()-100);
+		protMelee.revalidate();
+		protMelee.setTargetVerb("ProtPray");
+		protMelee.setName("Melee");
+		protMelee.setClickMask(WidgetConfig.USE_WIDGET | WidgetConfig.DRAG);
+		protMelee.setNoClickThrough(true);
+	}
+
+	private void removeMeleeWidget()
+	{
+		if (protMelee == null)
+		{
+			return;
+		}
+
+		Widget parent = protMelee.getParent();
+		if (parent == null)
+		{
+			return;
+		}
+
+		Widget[] children = parent.getChildren();
+		if (children == null || children.length <= protMelee.getIndex() || children[protMelee.getIndex()] != protMelee)
+		{
+			return;
+		}
+
+		children[protMelee.getIndex()] = null;
+	}
+
+	 */
+
+	
+
 	@Subscribe
 	private void onMenuOptionClicked(MenuOptionClicked event)
 	{
 		log.info(event.toString());
+		if(event.getOption().equals("action3") && event.getTarget().equals("button3")){
+			event.consume();
+			targetObject = utils.findNearestGameObject(29631);
+			if(targetObject!=null){
+				client.invokeMenuAction("","",29631,3,targetObject.getSceneMinLocation().getX(),targetObject.getSceneMinLocation().getY());
+			} else {
+				targetObject = utils.findNearestGameObject(29635);
+				if(targetObject!=null){
+					client.invokeMenuAction("","",29635,3,targetObject.getSceneMinLocation().getX(),targetObject.getSceneMinLocation().getY());
+				}
+			}
+			return;
+		} else if (event.getOption().equals("action2") && event.getTarget().equals("button2")){
+			event.consume();
+			client.invokeMenuAction("","",1,57,-1,14286991);
+			return;
+		} else if (event.getOption().equals("action1") && event.getTarget().equals("button1")){
+			event.consume();
+			targetNpc = utils.findNearestNpc(8132);
+			if(targetNpc!=null){
+				client.invokeMenuAction("","",targetNpc.getIndex(),9,0,0);
+			}
+			return;
+		}
 	}
 
 	private long sleepDelay()
