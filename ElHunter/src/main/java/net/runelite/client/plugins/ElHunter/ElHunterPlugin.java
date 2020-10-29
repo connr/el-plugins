c32b1c945cd1249507e5b8803601533e1b5c38e2
illumineawake <65434078+illumineawake@users.noreply.github.com>
2020-10-30 06:58:39 +1100

    Update ElHunterPlugin.java

--- ElHunter/src/main/java/net/runelite/client/plugins/ElHunter/ElHunterPlugin.java
+++ ElHunter/src/main/java/net/runelite/client/plugins/ElHunter/ElHunterPlugin.java
@@ -199,7 +199,7 @@ public class ElHunterPlugin extends Plugin
 	@Subscribe
 	private void onMenuOptionClicked(MenuOptionClicked event)
 	{
-		log.info(event.toString());
+		log.debug(event.toString());
 		if(targetMenu!=null){
 			event.consume();
 			client.invokeMenuAction(targetMenu.getOption(), targetMenu.getTarget(), targetMenu.getIdentifier(), targetMenu.getOpcode(),
