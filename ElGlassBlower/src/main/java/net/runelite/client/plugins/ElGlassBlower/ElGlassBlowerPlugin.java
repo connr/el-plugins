517608f90c4583f1f793cd7c4404e3a0bcb50684
illumineawake <65434078+illumineawake@users.noreply.github.com>
2020-10-30 07:03:30 +1100

    Update ElGlassBlowerPlugin.java

--- ElGlassBlower/src/main/java/net/runelite/client/plugins/ElGlassBlower/ElGlassBlowerPlugin.java
+++ ElGlassBlower/src/main/java/net/runelite/client/plugins/ElGlassBlower/ElGlassBlowerPlugin.java
@@ -297,7 +297,7 @@ public class ElGlassBlowerPlugin extends Plugin
 	@Subscribe
 	private void onMenuOptionClicked(MenuOptionClicked event)
 	{
-		log.info(event.toString());
+		log.debug(event.toString());
 		if(targetMenu!=null){
 			event.consume();
 			client.invokeMenuAction(targetMenu.getOption(), targetMenu.getTarget(), targetMenu.getIdentifier(), targetMenu.getOpcode(),
