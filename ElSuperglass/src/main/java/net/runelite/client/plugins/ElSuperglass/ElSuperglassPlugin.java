21cb165c64fad4322ffdfe6bf4955238aa66c261
illumineawake <65434078+illumineawake@users.noreply.github.com>
2020-10-30 07:00:40 +1100

    Update ElSuperglassPlugin.java

--- ElSuperglass/src/main/java/net/runelite/client/plugins/ElSuperglass/ElSuperglassPlugin.java
+++ ElSuperglass/src/main/java/net/runelite/client/plugins/ElSuperglass/ElSuperglassPlugin.java
@@ -230,7 +230,7 @@ public class ElSuperglassPlugin extends Plugin
 	@Subscribe
 	private void onMenuOptionClicked(MenuOptionClicked event)
 	{
-		log.info(event.toString());
+		log.debug(event.toString());
 		if(targetMenu!=null){
 			event.consume();
 			client.invokeMenuAction(targetMenu.getOption(), targetMenu.getTarget(), targetMenu.getIdentifier(), targetMenu.getOpcode(),
